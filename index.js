4de768bc94bb4e3489ad073c9d5d7bf5a9ffb0b7
coderDaiyan <daiyanabdallah@gmail.com>
2021-03-23 00:29:06 +0600

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+
+const users = ["Ashik", "Moin", "Jabed", "Shadhin", "Israfil"];
+
 app.get("/", (req, res) => {
-  res.send("My First API Request");
+  const fruit = {
+    product: "alu",
+    price: 200,
+  };
+  res.send(fruit);
+});
+
+app.get("/fruits/banana", (req, res) => {
+  res.send({ fruit: "banana", quantity: 500, price: 200 });
+});
+
+app.get("/user/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(req.query.sort);
+  const name = users[id];
+  res.send({ id, name });
 });
 
 app.listen(3000, () => console.log("Listening to Port 3000"));
